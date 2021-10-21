527290a6e72c83910b8018972930b01f5be491c5
Alexandro Boaventura <alexandro.boaventura@yahoo.com.br>
2021-10-21 16:05:44 -0300

    inclusão script

---
+++ script.js
@@ -0,0 +1,10 @@
+function showMSomething() {
+  alert('Amigo estou aqui')
+}
+
+const link = {
+  github: 'oalexandrob',
+  cinstagram: 'oalexandrob',
+  linkedin: 'alexandro-boaventura',
+  facebook: 'oalexandrob'
+}
