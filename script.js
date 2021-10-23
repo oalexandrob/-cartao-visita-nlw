0bc750cc4b6232298c9a6af7bdcc2333a3cab843
Alexandro Boaventura <alexandro.boaventura@yahoo.com.br>
2021-10-23 10:58:56 -0300

    correção dos links das redes sociais e ajuste viewport

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
     li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    // alert(`https://${social}.com/${linksSocialMedia[social]}`)
   }
 }
 
